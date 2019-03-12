c48f5b133ce60161e7c1f6bd3d72f6183b089fd9
tedi <tedi0722@users.noreply.github.com>
2019-03-11 20:13:42 -0700

    router dom fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Section from './Components/Section/Section';
@@ -7,14 +7,14 @@ import Section from './Components/Section/Section';
 class App extends Component {
   render() {
     return (
-      <Router basename="process.env.PUBLIC_URL">
+      <Route exact path="/" >
         <div className="App">
           <Navbar />
           <Section id="section1" title="Section 1 Title" paragraph="Section 1 Paragraph" />
           <Section id="section2" title="Section 2 Title" paragraph="Section 2 Paragraph" />
           <Section id="section3" title="Section 3 Title" paragraph="Section 3 Paragraph" />
         </div>
-      </Router>
+      </Route>
     );
   }
 }
